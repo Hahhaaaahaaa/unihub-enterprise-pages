8b62d6ed182625ca02ee4a4b0ddcda535d139bd4
Dotohua9 <Dotohua9@users.noreply.github.com>
2026-09-12 12:24:06 +0800

    deploy: unihub-20260912042136-df18bca synchronized application and SDK release

---
+++ assets/connect-BkCUvFLC.js
@@ -0,0 +1,1 @@
+import{t as e}from"./device-identity-DOFw1eNq.js";import{connectBytechMagnetic as t,waitForCool75Disconnect as n}from"./connect-B9f-Roji.js";var r=n,i=n=>t(n,{name:`COOL68`,vendorId:Number(e.vendorId),productId:Number(e.productId),uuid:Number(e.uuid)});export{i as connectKeyboard,r as waitForDisconnect};
